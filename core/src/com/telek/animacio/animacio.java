367675eaf03798cb7fae5d04474c6ada9f865a8c
JesusSePe <umbreon20019@gmail.com>
2022-02-12 18:42:36 +0100

    Animation done

--- core/src/com/telek/animacio/animacio.java
+++ core/src/com/telek/animacio/animacio.java
@@ -8,18 +8,19 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class animacio extends ApplicationAdapter {
 
 	// Constant rows and columns of the sprite sheet
-	private static final int FRAME_COLS = 10, FRAME_ROWS = 8;
+	private static final int FRAME_COLS = 10, FRAME_ROWS = 8, FRAME_IDLE = 3, FRAME_1 = 1;
 
 	// Objects used
 	Animation<TextureRegion> idleDownAnimation, idleLeftAnimation, idleUpAnimation, idleRightAnimation, walkDownAnimation, walkLeftAnimation, walkUpAnimation, walkRightAnimation; // Must declare frame type (TextureRegion)
 	Texture walkSheet;
 	SpriteBatch spriteBatch;
+	Texture back;
+	int posY, posX = 0;
 
 	// A variable for tracking elapsed time for the animation
 	float stateTime;
@@ -29,6 +30,9 @@ public class animacio extends ApplicationAdapter {
 
 		// Load the sprite sheet as a Texture
 		walkSheet = new Texture(Gdx.files.internal("SpriteSheetLink.png"));
+
+		// Load background
+		back = new Texture(Gdx.files.internal("maze.png"));
 
 		// Use the split utility method to create a 2D array of TextureRegions. This is
 		// possible because this sprite sheet contains frames of equal size and they are
@@ -41,45 +45,45 @@ public class animacio extends ApplicationAdapter {
 		// left, going across first. The Animation constructor requires a 1D array.
 		int index = 0;
 
-		TextureRegion[] idleDownFrames = new TextureRegion[FRAME_COLS];
-		for (int j = 0; j < FRAME_COLS; j++) {
-			idleDownFrames[index++] = tmp[0][2];
+		TextureRegion[] idleDownFrames = new TextureRegion[FRAME_IDLE];
+		for (int j = 0; j < FRAME_IDLE; j++) {
+			idleDownFrames[index++] = tmp[0][j];
 		}
 
-		TextureRegion[] idleLeftFrames = new TextureRegion[FRAME_COLS];
+		TextureRegion[] idleLeftFrames = new TextureRegion[FRAME_IDLE];
 		index = 0;
-		for (int j = 0; j < FRAME_COLS; j++) {
-			idleLeftFrames[index++] = tmp[1][2];
+		for (int j = 0; j < FRAME_IDLE; j++) {
+			idleLeftFrames[index++] = tmp[1][j];
 		}
 
-		TextureRegion[] idleUpFrames = new TextureRegion[FRAME_COLS];
+		TextureRegion[] idleUpFrames = new TextureRegion[FRAME_1];
 		index = 0;
-		for (int j = 0; j < FRAME_COLS; j++) {
-			idleUpFrames[index++] = tmp[2][0];
+		for (int j = 0; j < FRAME_1; j++) {
+			idleUpFrames[index++] = tmp[2][j];
 		}
 
-		TextureRegion[] idleRightFrames = new TextureRegion[FRAME_COLS];
+		TextureRegion[] idleRightFrames = new TextureRegion[FRAME_IDLE];
 		index = 0;
-		for (int j = 0; j < FRAME_COLS; j++) {
-			idleRightFrames[index++] = tmp[3][2];
+		for (int j = 0; j < FRAME_IDLE; j++) {
+			idleRightFrames[index++] = tmp[3][j];
 		}
 
 		TextureRegion[] walkDownFrames = new TextureRegion[FRAME_COLS];
 		index = 0;
 		for (int j = 0; j < FRAME_COLS; j++) {
-			walkDownFrames[index++] = tmp[4][FRAME_COLS];
+			walkDownFrames[index++] = tmp[4][j];
 		}
 
 		TextureRegion[] walkLeftFrames = new TextureRegion[FRAME_COLS];
 		index = 0;
 		for (int j = 0; j < FRAME_COLS; j++) {
-			walkLeftFrames[index++] = tmp[5][FRAME_COLS];
+			walkLeftFrames[index++] = tmp[5][j];
 		}
 
 		TextureRegion[] walkUpFrames = new TextureRegion[FRAME_COLS];
 		index = 0;
 		for (int j = 0; j < FRAME_COLS; j++) {
-			walkUpFrames[index++] = tmp[6][FRAME_COLS];
+			walkUpFrames[index++] = tmp[6][j];
 		}
 
 		TextureRegion[] walkRightFrames = new TextureRegion[FRAME_COLS];
@@ -89,14 +93,14 @@ public class animacio extends ApplicationAdapter {
 		}
 
 		// Initialize the Animations with the frame interval and array of frames
-		idleDownAnimation = new Animation<TextureRegion>(0.1f, idleDownFrames);
-		idleLeftAnimation = new Animation<TextureRegion>(0.1f, idleLeftFrames);
-		idleUpAnimation = new Animation<TextureRegion>(0.1f, idleUpFrames);
-		idleRightAnimation = new Animation<TextureRegion>(0.1f, idleRightFrames);
-		walkDownAnimation = new Animation<TextureRegion>(0.1f, walkDownFrames);
-		walkLeftAnimation = new Animation<TextureRegion>(0.1f, walkLeftFrames);
-		walkUpAnimation = new Animation<TextureRegion>(0.1f, walkUpFrames);
-		walkRightAnimation = new Animation<TextureRegion>(0.1f, walkRightFrames);
+		idleDownAnimation = new Animation<>(0.3f, idleDownFrames);
+		idleLeftAnimation = new Animation<>(0.3f, idleLeftFrames);
+		idleUpAnimation = new Animation<>(0.3f, idleUpFrames);
+		idleRightAnimation = new Animation<>(0.3f, idleRightFrames);
+		walkDownAnimation = new Animation<>(0.1f, walkDownFrames);
+		walkLeftAnimation = new Animation<>(0.1f, walkLeftFrames);
+		walkUpAnimation = new Animation<>(0.1f, walkUpFrames);
+		walkRightAnimation = new Animation<>(0.1f, walkRightFrames);
 
 		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
 		// time to 0
@@ -108,36 +112,40 @@ public class animacio extends ApplicationAdapter {
 	public void render() {
 		ScreenUtils.clear(1, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
 		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 
-		// Get current frame of animation for the current stateTime
-		TextureRegion currentFrame = idleDownAnimation.getKeyFrame(stateTime, true);
+		//TextureRegion currentFrame = idleDownAnimation.getKeyFrame(stateTime, true);
 		spriteBatch.begin();
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) animacio.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-		// Keep bucket inside screen.
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-		// Check if it's time to create new drops.
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-		// Move the raindrops.
-		for (Iterator<Rectangle> i = raindrops.iterator(); i.hasNext(); ) {
-			Rectangle raindrop = i.next();
-			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if(raindrop.y + 64 < 0) {
-				//i.remove();
-				rainMusic.dispose();
-				// Game over screen
-				game.setScreen(new GameOver(game, dropCount));
-			}
-			if(raindrop.overlaps(bucket) && raindrop.y > 80) {
-				System.out.println(raindrop.y);
-				dropCount += 1;
-				dropSound.play();
-				i.remove();
-			}
+
+		// Draw background
+		spriteBatch.draw(back, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		TextureRegion currentFrame;
+
+		// Moving
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && posX > Gdx.graphics.getWidth()){
+			currentFrame = walkLeftAnimation.getKeyFrame(stateTime, true);
+			posX += 200 * Gdx.graphics.getDeltaTime();
+
+		} else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && posX < 0){
+			currentFrame = walkRightAnimation.getKeyFrame(stateTime, true);
+			posX -= 200 * Gdx.graphics.getDeltaTime();
+
+		} else if(Gdx.input.isKeyPressed(Input.Keys.UP) && posY > Gdx.graphics.getHeight()){
+			currentFrame = walkUpAnimation.getKeyFrame(stateTime, true);
+			posY += 200 * Gdx.graphics.getDeltaTime();
+
+		} else if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && posY < 0){
+			currentFrame = walkDownAnimation.getKeyFrame(stateTime, true);
+			posY -= 200 * Gdx.graphics.getDeltaTime();
+
+		} else {
+			// Idle
+			currentFrame = idleDownAnimation.getKeyFrame(stateTime, true);
 		}
+		spriteBatch.draw(currentFrame, posX, posY);
+
+
+
 		spriteBatch.end();
 	}
 
